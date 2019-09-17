209d15073d115c0e5d58b3bec60044040e76160a
RedGiant <systeminate@gmail.com>
2019-09-17 14:44:48 +0800

    Instagram Rrails2

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,4 +6,24 @@ class PhotosController < ApplicationController
  	def new 
  		@photos = Photo.new
  	end
+
+ 	def create 
+ 		@photo = Photo.new(photo_params)
+ 	
+ 		if @photo.save 
+		 	redirect_to @photo
+
+		else
+			render :new
+		end
+ 	end
+ 	
+ 		def show
+ 			@photo = Photo.find(params[:id])
+ 		end
+
+ 	private 
+ 	def photo_params
+ 		params.require(:photo).permit(:caption)
+ 	end
  end
